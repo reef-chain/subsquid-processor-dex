4b3c6900362c380f4352b4d4b42db4035f0067bf
boo-0x <boo_0x@tutanota.com>
2023-04-11 13:46:17 +0200

    Merge migrations

--- db/migrations/1681213557568-Data.js
+++ db/migrations/1681213557568-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1681126198736 {
-    name = 'Data1681126198736'
+module.exports = class Data1681213557568 {
+    name = 'Data1681213557568'
 
     async up(db) {
         await db.query(`CREATE TABLE "pool" ("id" character varying NOT NULL, "evm_event_id" text, "token1" text NOT NULL, "token2" text NOT NULL, "pool_decimal" integer NOT NULL, "decimal1" integer NOT NULL, "decimal2" integer NOT NULL, "name1" text NOT NULL, "name2" text NOT NULL, "symbol1" text NOT NULL, "symbol2" text NOT NULL, "verified" boolean NOT NULL, CONSTRAINT "PK_db1bfe411e1516c01120b85f8fe" PRIMARY KEY ("id"))`)
@@ -7,11 +7,12 @@ module.exports = class Data1681126198736 {
         await db.query(`CREATE INDEX "IDX_d39ca709b11f93e38e867c1cec" ON "pool" ("token1") `)
         await db.query(`CREATE INDEX "IDX_1241dca5be35855e64fbe17c6a" ON "pool" ("token2") `)
         await db.query(`CREATE INDEX "IDX_548fd369b1d644edd67cfbd25b" ON "pool" ("verified") `)
-        await db.query(`CREATE TABLE "pool_event" ("id" character varying NOT NULL, "to_address" text, "sender_address" text, "signer_address" text, "hash" text, "type" character varying(8) NOT NULL, "amount1" numeric, "amount2" numeric, "amount_in1" numeric, "amount_in2" numeric, "reserved1" numeric, "reserved2" numeric, "supply" numeric, "total_supply" numeric, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "pool_id" character varying, CONSTRAINT "PK_d84dbf06888f1aca5ee6501c700" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE TABLE "pool_event" ("id" character varying NOT NULL, "to_address" text, "sender_address" text, "signer_address" text, "block_height" integer NOT NULL, "index_in_block" integer NOT NULL, "type" character varying(8) NOT NULL, "amount1" numeric, "amount2" numeric, "amount_in1" numeric, "amount_in2" numeric, "reserved1" numeric, "reserved2" numeric, "supply" numeric, "total_supply" numeric, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "pool_id" character varying, CONSTRAINT "PK_d84dbf06888f1aca5ee6501c700" PRIMARY KEY ("id"))`)
         await db.query(`CREATE INDEX "IDX_eb1548c8a81dc16b4ae985e80d" ON "pool_event" ("pool_id") `)
         await db.query(`CREATE INDEX "IDX_b9019ae9bf01c1222b8fd6f05b" ON "pool_event" ("to_address") `)
         await db.query(`CREATE INDEX "IDX_bbc0bd39c7c5272d03de336227" ON "pool_event" ("sender_address") `)
         await db.query(`CREATE INDEX "IDX_3048275fa846b084bb1cd106b2" ON "pool_event" ("signer_address") `)
+        await db.query(`CREATE INDEX "IDX_0606826978378661ed61b308e2" ON "pool_event" ("block_height") `)
         await db.query(`CREATE INDEX "IDX_5ae3939673dbcacd64b4d096a7" ON "pool_event" ("type") `)
         await db.query(`CREATE INDEX "IDX_f1e08873fd8b0457f07fa06c3b" ON "pool_event" ("amount1") `)
         await db.query(`CREATE INDEX "IDX_596b79cbd7dbb34a3bb884b837" ON "pool_event" ("amount2") `)
@@ -54,6 +55,7 @@ module.exports = class Data1681126198736 {
         await db.query(`DROP INDEX "public"."IDX_b9019ae9bf01c1222b8fd6f05b"`)
         await db.query(`DROP INDEX "public"."IDX_bbc0bd39c7c5272d03de336227"`)
         await db.query(`DROP INDEX "public"."IDX_3048275fa846b084bb1cd106b2"`)
+        await db.query(`DROP INDEX "public"."IDX_0606826978378661ed61b308e2"`)
         await db.query(`DROP INDEX "public"."IDX_5ae3939673dbcacd64b4d096a7"`)
         await db.query(`DROP INDEX "public"."IDX_f1e08873fd8b0457f07fa06c3b"`)
         await db.query(`DROP INDEX "public"."IDX_596b79cbd7dbb34a3bb884b837"`)
